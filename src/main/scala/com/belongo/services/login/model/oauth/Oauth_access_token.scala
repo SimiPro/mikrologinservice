e0213e48b05ad40d4d9138e40de4b4ff658c7e0d
Simipro <sihuber@gmx.net>
2015-06-26 22:24:43 +0200

    Store Tokens

---
+++ src/main/scala/com/belongo/services/login/model/oauth/Oauth_access_token.scala
@@ -0,0 +1,29 @@
+package com.belongo.services.login.model.oauth
+
+import java.sql.Blob
+import javax.persistence.{Id, Entity}
+
+import scala.beans.BeanProperty
+
+/**
+ * Created by Simon on 26.06.2015.
+ */
+@Entity
+class Oauth_access_token {
+  @Id
+  @BeanProperty
+  var token_id:String = _
+  @BeanProperty
+  var token:Blob = _
+  @BeanProperty
+  var authentication_id:String = _
+  @BeanProperty
+  var user_name:String = _
+  @BeanProperty
+  var client_id:String = _
+  @BeanProperty
+  var authentication:Blob = _
+  @BeanProperty
+  var refresh_token:String = _
+
+}
---
+++ src/main/scala/com/belongo/services/login/model/oauth/Oauth_client_token.scala
@@ -0,0 +1,24 @@
+package com.belongo.services.login.model.oauth
+
+import java.sql.Blob
+import javax.persistence.{Id, Entity}
+
+import scala.beans.BeanProperty
+
+/**
+ * Created by Simon on 26.06.2015.
+ */
+@Entity
+class Oauth_client_token {
+  @Id
+  @BeanProperty
+  var token_id:String = _
+  @BeanProperty
+  var token:Array[Byte] = _
+  @BeanProperty
+  var authentication_id:String = _
+  @BeanProperty
+  var user_name:String = _
+  @BeanProperty
+  var client_id:String = _
+}
