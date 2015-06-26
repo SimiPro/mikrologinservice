7a693fd7ca0f01a7617e9db3d1e0169ea20fe695
Simipro <sihuber@gmx.net>
2015-06-26 22:24:20 +0200

    Store Tokens

---
+++ src/main/scala/com/belongo/services/login/model/oauth/ClientDetails.scala
@@ -0,0 +1,35 @@
+package com.belongo.services.login.model.oauth
+
+import javax.persistence.{Id, Entity}
+
+import scala.beans.BeanProperty
+
+/**
+ * Created by Simon on 26.06.2015.
+ */
+@Entity
+class ClientDetails {
+  @Id
+  @BeanProperty
+  var appId:String = _
+  @BeanProperty
+  var resourceIds:String = _
+  @BeanProperty
+  var scope:String = _
+  @BeanProperty
+  var appSecret:String = _
+  @BeanProperty
+  var grantTypes:String = _
+  @BeanProperty
+  var redirectUrl:String = _
+  @BeanProperty
+  var authorities:String = _
+  @BeanProperty
+  var access_token_validity:Int = _
+  @BeanProperty
+  var refresh_token_validity:Int = _
+  @BeanProperty
+  var additionalInformation:String = _
+
+
+}
